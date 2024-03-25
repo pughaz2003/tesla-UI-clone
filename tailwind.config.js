eba678efaa54c24e484199c77fe1cb9627fe2837
pughaz2003 <pughaz668@gmail.com>
2024-03-25 20:37:06 +0530

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./tesla.html"],
+theme: {
+    extend: {
+       colors:{
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+       }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar') 
+  ],
+}
+
